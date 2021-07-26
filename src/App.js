7a0d283e91d9d778462fb623688aa75e112c3874
vada109 <vada109@gmail.com>
2021-07-26 20:41:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : 'JSLEE',
   'birthday' : '760519',
-  'gender' : 'Man',
-  'job' : 'Student'
-}
+  'gender' : '남자',
+  'job' : '학생'
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '이순신',
+  'birthday' : '880723',
+  'gender' : '남자',
+  'job' : '군인'
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '홍길동',
+  'birthday' : '950204',
+  'gender' : '여자',
+  'job' : '회사원'
+  }
+]
 
 function App() {
   return (
-    <Customer 
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}
-    />
+    /* 
+    //반복문을 사용해서 내용을 줄인다. map 사용
+    <div>
+      <Customer
+        id = {customers[0].id}
+        image = {customers[0].image}
+        name = {customers[0].name}
+        birthday = {customers[0].birthday}
+        gender = {customers[0].gender}
+        job = {customers[0].job}
+      />
+      <Customer
+        id = {customers[1].id}
+        image = {customers[1].image}
+        name = {customers[1].name}
+        birthday = {customers[1].birthday}
+        gender = {customers[1].gender}
+        job = {customers[1].job}
+      />
+      <Customer
+        id = {customers[2].id}
+        image = {customers[2].image}
+        name = {customers[2].name}
+        birthday = {customers[2].birthday}
+        gender = {customers[2].gender}
+        job = {customers[2].job}
+      />
+    </div>
+    */ 
+
+    //map를 사용할 경우 만든시 key를 설정해야 한다.
+    <div>
+      {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+    </div>
   );
 }  
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,9 +3,29 @@ import React from 'react';
 class Customer extends React.Component{
     render () {
         return (
-            //props를 이용하여 전달받은 값을 출력할 수 있도록 한다.
             <div>
-                <h2>{this.props.name}</h2>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component{
+    render () {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render () {
+        return (
+            <div>
                 <p>{this.props.birthday}</p>
                 <p>{this.props.gender}</p>
                 <p>{this.props.job}</p>
@@ -13,5 +33,4 @@ class Customer extends React.Component{
         )
     }
 }
-
 export default Customer;
